9ebcf544e82563aeb01622b9f7aa3c5fdc931eb3
KevinLatino <latinokevin9@gmail.com>
2024-05-05 12:52:43 -0600

    Se agrega estilos a CodeComponent

--- src/Components/CodeComponent/CodeComponent.jsx
+++ src/Components/CodeComponent/CodeComponent.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeComponent = ({ codeString }) => {
-    return (
-        <>
-                <SyntaxHighlighter language="javascript" style={atomOneDark}>
-                    {codeString}
-                </SyntaxHighlighter>
-        </>
-    )
-}
 
-export { CodeComponent }
+    const [lenguaje, setLenguaje] = useState(false);
+
+    const isShowing = () => {
+        setLenguaje(true)
+    }
+
+    const isLeaving = () => {
+        setLenguaje(false)
+    }
+
+    return (
+        <div
+            className='relative'
+            onMouseEnter={isShowing}
+            onMouseLeave={isLeaving}
+        >
+            <SyntaxHighlighter language='javascript' style={atomOneDark}>
+                {codeString}
+            </SyntaxHighlighter>
+            {lenguaje && (
+                <div className='cursor-pointer absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-75 text-white'>
+                    JavaScript
+                </div>
+            )}
+        </div>
+    );
+};
+
+export { CodeComponent };
+
