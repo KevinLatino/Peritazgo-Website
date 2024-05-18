f0cf04f89647dc310bc6e549746eb73b36839d15
KevinLatino <latinokevin9@gmail.com>
2024-05-17 22:34:06 -0600

    git config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/Revista-Peritazgo-2024/"
+  plugins: [react()]
 })
