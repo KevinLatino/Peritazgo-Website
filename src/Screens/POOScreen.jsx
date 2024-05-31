943745e926398c6938d3e4c5d46a8ff50ff968ff
KevinLatino <latinokevin9@gmail.com>
2024-05-31 15:02:48 -0600

    Adding info abou classes, methods, Objects and instances

--- src/Screens/POOScreen.jsx
+++ src/Screens/POOScreen.jsx
@@ -1,14 +1,114 @@
 import React from 'react'
-import { Title, Paragraph } from '../Components/PageTextsComponent'
+import { Title, Paragraph, Space, BlueTitle, PurpleTitle, GreenTitle, RedTitle } from '../Components/PageTextsComponent'
+import { BlueSection, GreenSection, PurpleSection, RedSection } from '../Components/SectionsComponent'
+import { BackButton } from '../Components/BackButtonComponent'
+import { CodeComponent } from '../Components/CodeComponent'
 
 const POOScreen = () => {
+
+    const stringClassBasic =
+        `class Animal { //aquí se declara la clase
+  constructor(name, specie){ //Son Atributos de la clase
+   this.name = name;
+   this.specie = specie;
+ };
+};`
+
+    const stringMethod =
+        `class Animal { //aquí se declara la clase
+   constructor(name, specie){ //Atributos de la clase
+    this.name = name;
+    this.specie = specie;
+ };
+ makeSound(){ //aquí se declara la clase
+    console.log(this.name + ' make a sound');
+ };
+};`
+
+    const stringObject =
+        `class Animal { // aquí se declara la clase
+    constructor(name, specie) { // Atributos de la clase
+    this.name = name;
+    this.specie = specie;
+ };
+makeSound() { // Método de la clase
+console.log(this.name + ' makes a sound');
+ }
+}
+// Crear una instancia de la clase Animal
+const dog = new Animal('Figo', 'Dog');
+// Llamar al método makeSound en la instancia dog
+dog.makeSound(); // Figo makes a sound`
+
     return (
         <>
             <div className='animate__animated animate__fadeInLeft'>
                 <Title>
-                    Paradigma Orientado a Objetos
+                    Programación Orientada a Objetos
                 </Title>
+                <Paragraph>
+                    Es un paradigma de programación, es decir, un modelo o un estilo de programación. Se basa en el concepto de clases y objetos. Este tipo de programación
+                    se utiliza para estructurar un programa de software en piezas simples y reutilizables.
+                </Paragraph>
+                <Space />
+
+                <Title>
+                    Clases, métodos, objetos e instancias
+                </Title>
+                <Space />
+
+                <div className='flex gap-8 '>
+                    <BlueSection>
+                        <BlueTitle>
+                            Clases
+                        </BlueTitle>
+                        <Paragraph>
+                            Es una plantilla que define las propiedades (atributos) y comportamientos (métodos) que tendrán los objetos creados a partir de ella.
+                        </Paragraph>
+                        <Space />
+                        <CodeComponent codeString={stringClassBasic} />
+                    </BlueSection>
+
+                    <PurpleSection>
+                        <PurpleTitle>
+                            Métodos
+                        </PurpleTitle>
+                        <Paragraph>
+                            Es una función definida dentro de una clase. Describe una acción que los objetos de esa clase pueden realizar.
+                        </Paragraph>
+                        <Space />
+                        <CodeComponent codeString={stringMethod} />
+                    </PurpleSection>
+                </div>
+
+                <Space />
+
+                <div className='flex gap-8'>
+                    <GreenSection>
+                        <GreenTitle>
+                            Objetos
+                        </GreenTitle>
+                        <Paragraph>
+                            Un objeto es una entidad concreta que se crea a partir de una clase y que contiene tanto datos (atributos) como comportamientos (métodos). Es una instancia de una clase.
+                        </Paragraph>
+                        <Space />
+                        <CodeComponent codeString={stringObject} />
+                    </GreenSection>
+
+                    <RedSection>
+                        <RedTitle>
+                            Instancias
+                        </RedTitle>
+
+                        <Paragraph>
+                            Es el proceso de crear un objeto a partir de una clase. Es cuando se usa una clase para crear un objeto específico.
+                        </Paragraph>
+                        <Space />
+                        <CodeComponent codeString={stringObject} />
+                    </RedSection>
+                </div>
             </div>
+            <BackButton />
         </>
     )
 }
